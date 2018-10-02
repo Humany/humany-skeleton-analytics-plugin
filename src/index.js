b1e7d2de3bcdb72dc820ba75867cdc8d283efc64
Markus Hederström <markus.hederstrom@humany.com>
2018-10-02 15:31:51 +0200

    using new tracking api with analyzers

--- src/index.js
+++ src/index.js
@@ -1,24 +1,39 @@
-import { AnalyticsPlugin } from '@humany/widget-tracking';
+import { TrackingPlugin } from '@humany/widget-tracking';
 
 import { callbacks, setup } from './shared';
 
 setup();
 
-const humany = window.Humany;
+const humany = window.humany;
 
-humany.configure('*', config => {
-  config.plugin(AnalyticsPlugin, {
-    action: ({ type, args, location })  => {
-      console.log('ACTION DISPATCHED', type, args, location);
+class CustomAnalyzer {
+  constructor(container, emit) {
+    const { events } = container.get('$widget');
+
+    events.subscribe('widget:move-outside-viewport', (e, data) => {
+      emit('MovedOutsideViewport', () => Promise.resolve({
+        message: 'moved outside viewport',
+      }));
+    });
+  }
+}
+
+humany.configure(config => {
+  config.plugin(TrackingPlugin, {
+    action: ({ type, resolve })  => {
+      resolve().then(data => {
+        console.log('ACTION DISPATCHED', type, data);
+
+        if (typeof invokable === 'function') {
+          invokable(data);
+        }
+      }).catch(error => {
+        console.log('error in tracking plugin', error);
+      });
 
       const invokable = callbacks[type];
 
-      if (typeof invokable === 'function') {
-        invokable(args);
-      }
     },
-    navigate: ({ type, args }) => {
-      console.log('NAVIGATE DISPATCHED', type, args, location);
-    },
+    analyzers: [CustomAnalyzer],
   });
 });
