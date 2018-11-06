babefd9b9c363eecfb89dcd11f33a8b9b996f30e
Markus Hederström <markus.hederstrom@humany.com>
2018-11-06 13:34:01 +0100

    fix: works with updated google analytics api

--- src/index.js
+++ src/index.js
@@ -1,39 +1,14 @@
-import { TrackingPlugin } from '@humany/widget-tracking';
-
-import { callbacks, setup } from './shared';
-
-setup();
+import { GoogleAnalyticsPlugin } from '@humany/widget-analytics';
 
 const humany = window.humany;
 
-class CustomAnalyzer {
-  constructor(container, emit) {
-    const { events } = container.get('$widget');
-
-    events.subscribe('widget:move-outside-viewport', (e, data) => {
-      emit('MovedOutsideViewport', () => Promise.resolve({
-        message: 'moved outside viewport',
-      }));
-    });
-  }
-}
-
 humany.configure(config => {
-  config.plugin(TrackingPlugin, {
+  config.plugin(GoogleAnalyticsPlugin, {
     action: ({ type, resolve })  => {
-      resolve().then(data => {
-        console.log('ACTION DISPATCHED', type, data);
-
-        if (typeof invokable === 'function') {
-          invokable(data);
-        }
-      }).catch(error => {
-        console.log('error in tracking plugin', error);
-      });
-
-      const invokable = callbacks[type];
-
+      resolve()
+        .then(data => {
+          console.log('Action emitted:', type, data);
+        });
     },
-    analyzers: [CustomAnalyzer],
   });
 });
