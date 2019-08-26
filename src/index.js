ab5b069272d853db4decaf44d358199361197f50
Markus Hederström <markus.hederstrom@humany.com>
2019-08-26 10:59:30 +0200

    add widget-analytics package

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import { GoogleAnalyticsPlugin } from '@humany/widget-analytics';
 
 const humany = window.humany;
 
+if (!humany) {
+  console.error('No Humany installation is available on the page.');
+}
+
 humany.configure(config => {
   config.plugin(GoogleAnalyticsPlugin, {
     action: ({ type, resolve })  => {
